Fix this calculation to add up the array

powerbi_survey_charts/diverging_stacked_barchart.py
@@ -40,8 +40,8 @@ def survey(results, category_names):
     middle_index = data.shape[1] // 2
     offsets = data[:, range(middle_index)].sum(axis=1) + data[:, middle_index] / 2
 
-    lhs_max = max([counts[:middle_index] + counts[middle_index] / 2 for counts in results.values()])
-    rhs_max = max([counts[middle_index+1:] + counts[middle_index] / 2 for counts in results.values()])
+    lhs_max = max([sum(counts[:middle_index]) + counts[middle_index] / 2 for counts in results.values()])
+    rhs_max = max([sum(counts[middle_index+1:]) + counts[middle_index] / 2 for counts in results.values()])
     lhs_max = lhs_max + (5 - lhs_max % 5) # round off to the nearest 5
     rhs_max = rhs_max + (5 - rhs_max % 5) # round off to the nearest 5
 